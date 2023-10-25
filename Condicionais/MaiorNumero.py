d84505d42588689f696b6219b46252cd001da464
ThomasVictorr <thomasvictor8741@gmail.com>
2023-10-24 21:09:58 -0300

    Update MaiorNumero.py

--- Condicionais/MaiorNumero.py
+++ Condicionais/MaiorNumero.py
@@ -1,1 +1,9 @@
-print('primeiro código python no github')
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+print('=-' * 10)
+
+if n1 > n2:
+    print(f'O maior número é {n1}')
+else:
+    print(f'O maior número é {n2}')
